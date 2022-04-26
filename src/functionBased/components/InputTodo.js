6b0175191d67de5175b65ecec68d93f3f1bf1622
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-04-26 17:31:29 -0400

    Fix all Linter Errors

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,29 +1,32 @@
-import React, { useState } from "react"
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { FaPlusCircle } from 'react-icons/fa';
 
-const InputTodo = props => {
+const InputTodo = (props) => {
   const [inputText, setInputText] = useState({
-    title: "",
-  })
+    title: '',
+  });
 
-  const onChange = e => {
+  const { addTodoProps } = props;
+
+  const onChange = (e) => {
     setInputText({
       ...inputText,
       [e.target.name]: e.target.value,
-    })
-  }
+    });
+  };
 
-  const handleSubmit = e => {
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (inputText.title.trim()) {
-      props.addTodoProps(inputText.title)
+      addTodoProps(inputText.title);
       setInputText({
-        title: "",
-      })
+        title: '',
+      });
     } else {
-      alert("Please write item")
+      alert('Please write item');
     }
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit} className="form-container">
@@ -35,13 +38,17 @@ const InputTodo = props => {
         name="title"
         onChange={onChange}
       />
-      <button className="input-submit">
+      <button type="button" className="input-submit">
         <FaPlusCircle
-          style={{ color: "darkcyan", fontSize: "20px", marginTop: "2px" }}
+          style={{ color: 'darkcyan', fontSize: '20px', marginTop: '2px' }}
         />
       </button>
     </form>
-  )
-}
+  );
+};
+
+InputTodo.propTypes = {
+  addTodoProps: PropTypes.func.isRequired,
+};
 
 export default InputTodo;
--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -4,50 +4,59 @@ import { MdClose } from 'react-icons/md';
 import { FiMenu } from 'react-icons/fi';
 
 const Navbar = () => {
-
-  const [navbarOpen, setNavbarOpen] = useState(false)
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
   const links = [
     {
       id: 1,
-      path: "/",
-      text: "Home",
+      path: '/',
+      text: 'Home',
     },
     {
       id: 2,
-      path: "/about",
-      text: "About",
+      path: '/about',
+      text: 'About',
     },
   ];
 
   const handleToggle = () => {
-    setNavbarOpen(prev => !prev)
-  }
+    setNavbarOpen((prev) => !prev);
+  };
 
   const closeMenu = () => {
-    setNavbarOpen(false)
-  }
+    setNavbarOpen(false);
+  };
 
-  return <nav className="navBar">
-    <button onClick={handleToggle}> {navbarOpen ? (
-      <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
-    ) : (
-      <FiMenu style={{ color: "#7b7b7b", width: "40px", height: "40px" }} />
-    )}</button>
-    <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
-      {links.map(link => {
-        return (
+  return (
+    <nav className="navBar">
+      <button type="button" onClick={handleToggle}>
+        {
+          navbarOpen ? (
+            <MdClose style={{ color: '#fff', width: '40px', height: '40px' }} />
+          ) : (
+            <FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />
+          )
+        }
+      </button>
+      <ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
+        {links.map((link) => (
           <li key={link.id}>
-            <NavLink className={(navData) => navData.isActive ? 'active-link' : ''}
+            <NavLink
+              className={(navData) => {
+                if (navData.isActive) {
+                  return 'active-link';
+                }
+                return '';
+              }}
               to={link.path}
               onClick={() => closeMenu()}
             >
               {link.text}
             </NavLink>
           </li>
-        )
-      })}
-    </ul>
-  </nav>
-}
-export default Navbar
+        ))}
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
